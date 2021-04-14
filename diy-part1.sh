c3d66c6d4b51b9e7f68963c777cc37925459227f
killer <77133451+990052053@users.noreply.github.com>
2021-04-14 16:22:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,23 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# Add custom feeds source into feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+
+# Clone custom feeds source
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb #git lua-maxminddb 依赖
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
+
+# Add custom theme
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+git clone https://github.com/sirpdboy/luci-theme-opentopd package/lean/luci-theme-opentopd
+git clone https://github.com/rosywrt/luci-theme-rosy.git package/lean/luci-theme-rosy
+git clone https://github.com/sypopo/luci-theme-argon-mc.git package/lean/luci-theme-argon-mc
+git clone https://github.com/Aslin-Ameng/luci-theme-Night.git package/lean/luci-theme-Night
+git clone https://github.com/Aslin-Ameng/luci-theme-Light.git package/lean/luci-theme-Light
+
+
